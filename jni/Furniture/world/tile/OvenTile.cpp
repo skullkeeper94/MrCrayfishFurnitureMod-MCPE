41c6539fbde04896c975ea8c79f647e93dd8f7fc
skullkeeper94 <gabubble2002@live.com>
2015-07-23 04:47:10 -0400

    Added collisionhelper back
    Forgot to upload this XD.

--- jni/Furniture/world/tile/OvenTile.cpp
+++ jni/Furniture/world/tile/OvenTile.cpp
@@ -14,7 +14,14 @@ OvenTile::OvenTile(int id, Material const* material) : RotatableTile(id, materia
 
 void OvenTile::addAABBs(TileSource* region, int x, int y, int z, AABB const* posAABB, std::vector<AABB, std::allocator<AABB>>& pool) {
 	addAABB(AABB(0.0F, 0.0F, 0.1F, 1.0F, 1.0F, 0.9F).move(x, y, z), posAABB, pool);
+        addAABB(CollisionHelper::getRotatedCollisionBox(region->getData(x, y, z), 0.9F, 1.0F, 0.1F, 1.0F, 1.2F, 0.9F).move(x, y, z), posAABB, pool);
+     addAABB(CollisionHelper::getRotatedCollisionBox(region->getData(x, y, z), -0.1F, 0.1F, 0.2F, 0.0F, 0.9F, 0.8F).move(x, y, z), posAABB, pool);
+     addAABB(CollisionHelper::getRotatedCollisionBox(region->getData(x, y, z), 0.5F, 1.0F, 0.2F, 0.7F, 1.1F, 0.4F).move(x, y, z), posAABB, pool);
+     addAABB(CollisionHelper::getRotatedCollisionBox(region->getData(x, y, z), 0.5F, 1.0F, 0.2F, 0.7F, 1.1F, 0.4F).move(x, y, z), posAABB, pool);
+     addAABB(CollisionHelper::getRotatedCollisionBox(region->getData(x, y, z), 0.1F, 1.0F, 0.2F, 0.3F, 1.1F, 0.4F).move(x, y, z), posAABB, pool);
+     addAABB(CollisionHelper::getRotatedCollisionBox(region->getData(x, y, z), 0.1F, 1.0F, 0.6F, 0.3F, 1.1F, 0.8F).move(x, y, z), posAABB, pool);
 }
+
 bool OvenTile::use(Player* player, int x, int y, int z) {
 	return true;
 }

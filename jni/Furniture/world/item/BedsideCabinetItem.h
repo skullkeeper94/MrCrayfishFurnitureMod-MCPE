0055b66077a06e643d7f7a7fd0b858f2fea70c95
skullkeeper94 <gabubble2002@live.com>
2015-07-23 05:15:07 -0400

    BedsideCabinetItem.h added

---
+++ jni/Furniture/world/item/BedsideCabinetItem.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "PlaceableItem.h"
+#include "../tile/BedsideCabinetTile.h"
+
+class BedsideCabinetItem : public PlaceableItem {
+public:
+    static int _id;
+
+    BedsideCabinetItem(int);
+};
